4f54209e066afe9d537892410d9aa8601ff3d617
Chloe Rice <chloe.mi.rice@gmail.com>
2016-11-15 14:34:05 -0500

    page skeletons done

---
+++ test/page.model.spec.js
@@ -0,0 +1,29 @@
+const chai = require('chai');
+const expect = chai.expect;
+
+describe('Page Model Tests', function () {
+
+  describe('Virtuals', function () {
+    it('formats and populates urlTitle value based on title value', function () {
+      
+    });
+  });
+
+
+  it('gets page with properly formatted route', function () {
+
+  });
+
+  it('gets and renders formatted page content', function () {
+
+  });
+
+  it('can find pages by tag', function () {
+
+  });
+
+  it('can find similar pages based on tags and id', function () {
+
+  });
+
+});
