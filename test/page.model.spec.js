b04a1bc492862a98e9e30b62b38f2ece89e095e3
Chloe Rice <chloe.mi.rice@gmail.com>
2016-11-15 15:24:56 -0500

    virtuals tests done

--- test/page.model.spec.js
+++ test/page.model.spec.js
@@ -1,23 +1,52 @@
 const chai = require('chai');
 const expect = chai.expect;
+const models = require('../models')
+const Page = models.Page;
 
 describe('Page model', function () {
 
   describe('Virtuals', function () {
 
+    beforeEach(function () {
+      var page = Page.build();
+      page.urlTitle = "Pancake_Batter";
+    });
+
     describe('route', function () {
-      it('returns the url_name prepended by "/wiki/"');
+      it('returns the url_name prepended by "/wiki/"', function () {
+        expect(page.urlTitle).to.be.equal('/wiki/Pancake_Batter');
+      });
     });
 
     describe('renderedContent', function () {
-      it('converts the markdown-formatted content into HTML');
+      it('converts the markdown-formatted content into HTML', function () {
+        var marked = chai.spy(marked);
+        expect(marked).to.have.been.called.with('page.content');
+      });
     });
 
   });
 
   describe('Class methods', function () {
+    beforeEach(function (done) {
+      Page.create({
+        title: 'foo',
+        content: 'bar',
+        tags: ['foo', 'bar']
+      })
+      .then(function () {
+        done();
+      })
+      .catch(done);
+      });
+
     describe('findByTag', function () {
-      it('gets pages with the search tag');
+      it('gets pages with the search tag', function () {
+        
+        expect()
+      });
+
+
       it('does not get pages without the search tag');
     });
   });
